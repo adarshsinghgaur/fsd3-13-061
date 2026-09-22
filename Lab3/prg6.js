62b9e0fb47769258d40a309f2c9b87697f8db136
Adarsh Singh gaur <adarshsinghgaur2007@gmail.com>
2026-09-22 15:12:54 +0530

    delete products

--- Lab3/prg6.js
+++ Lab3/prg6.js
@@ -1,49 +1,104 @@
 import http from "http";
-import { getAllProducts } from "./products.js";
+import { getAllProducts, addProduct, deleteProduct } from "./product.js";
+
 const server = http.createServer((req, res) => {
-    if (req.url === "/api/v1/products" && req.method === "GET") {
-        res.statusCode = 200;
-        const data = getAllProducts();
-        res.setHeader("content-type", "application/json");
-        res.end(
-            JSON.stringify({
-                count: data.length,
-                data,
-            }),
-        );
-    
-  } else if (req.url === "/api/v1/products" && req.method === "POST") {
-    // console.log("Request:",req);
-    let body = "";
-    req.on("data", (chunk) => {
-      body += chunk;
-    });
-    req.on("end", () => {
-      const product = JSON.parse(body);
-      console.log("received product:", product);
-      res.statusCode = 201;
-      res.end(JSON.stringify({ msg: "product added", product }));
-    });
-  } else if (req.url.startsWith("/products/") && req.method === "PUT") {
-    const productID = req.url.split("/").pop();
-    console.log("Update Product id:", productID);
-    let body = "";
-    req.on("data", (chunk) => {
-      body += chunk;
-    });
-    req.on("end", () => {
-      const product = JSON.parse(body);
-      product.id = productID;
-      res.statusCode = 200;
-      res.end(JSON.stringify({ msg: "product updated", product }));
-    });
-  } else if (req.url === "/" && req.method === "DELETE") {
+  // GET
+  if (req.url === "/api/v1/products" && req.method === "GET") {
     res.statusCode = 200;
-    res.end("DELETE Request");
-  } else {
+
+    const data = getAllProducts();
+
+    res.setHeader("content-type", "application/json");
+
+    res.end(
+      JSON.stringify({
+        count: data.length,
+        data,
+      }),
+    );
+  }
+
+  // POST
+  else if (req.url === "/api/v1/products" && req.method === "POST") {
+    let body = "";
+
+    req.on("data", (chunk) => {
+      body += chunk;
+    });
+
+    req.on("end", () => {
+      const product = JSON.parse(body);
+
+      const item = addProduct(product);
+
+      res.statusCode = 201;
+
+      res.end(
+        JSON.stringify({
+          msg: "product added",
+          data: item,
+        }),
+      );
+    });
+  }
+
+  // PUT
+  else if (req.url.startsWith("/api/v1/products/") && req.method === "PUT") {
+    const productID = req.url.split("/").pop();
+
+    console.log("Update Product id:", productID);
+
+    let body = "";
+
+    req.on("data", (chunk) => {
+      body += chunk;
+    });
+
+    req.on("end", () => {
+      const product = JSON.parse(body);
+
+      console.log("Updated Product:", product);
+
+      res.statusCode = 200;
+
+      res.end(
+        JSON.stringify({
+          msg: "product updated",
+          product,
+        }),
+      );
+    });
+  }
+
+  // DELETE
+  else if (req.url.startsWith("/api/v1/products/") && req.method === "DELETE") {
+    const pid = Number(req.url.split("/").pop());
+
+    res.statusCode = 200;
+
+    if (deleteProduct(pid)) {
+      res.end(
+        JSON.stringify({
+          msg: "item deleted",
+        }),
+      );
+    } else {
+      res.end(
+        JSON.stringify({
+          msg: `product with id ${pid} not found`,
+        }),
+      );
+    }
+  }
+
+  // Invalid request
+  else {
     res.statusCode = 404;
+
     res.end("request not found");
   }
 });
 
-server.listen(5000, () => console.log("prg6 is running"));
+server.listen(5001, () =>
+  console.log("prg6 is running on http://localhost:5001/"),
+);
--- Lab3/products.js
+++ Lab3/products.js
@@ -19,4 +19,19 @@ export const getAllProducts = () => {
   return products;
 };
 
-export const addProduct = () => {};
+export const addProduct = (item) => {
+  item.id = nextId;
+  nextId++;
+  products.push(item);
+  return item;
+};
+
+export const deleteProduct = (pid) => {
+  const item = products.findIndex((prd) => prd.id === pid);
+
+  if (item === -1) return false;
+
+  products.splice(item, 1);
+  console.log("products remaining:", products);
+  return true;
+};
