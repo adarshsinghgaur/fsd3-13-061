4d3c1f09c53c11f5132bb6acb0193e8079f5a63e
Adarsh Singh gaur <adarshsinghgaur2007@gmail.com>
2026-09-23 12:20:53 +0530

    express start

---
+++ Lab4/prg1.js
@@ -0,0 +1,10 @@
+import express from "express";
+
+const app = express();
+
+
+// request goes here
+app.get("/",(req,res)=>{
+    res.send("<h1>Hello Express</h1>")
+})
+app.listen(3333,() => console.log("prg1 is running at 3333"));
